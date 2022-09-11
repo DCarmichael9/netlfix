5aa14d41a14acd2d4b656bea41bcf6926eb9697b
Doris Carmichael <dekanem87@gmail.com>
2022-09-11 04:16:26 -0700

    demo3

--- ec2.tf
+++ ec2.tf
@@ -2,4 +2,4 @@
 # type --> t2.xlarge
 #OS --> Windows
 # added ebs volume 1000 GB
-# Doris, another night! you went in on git and git hub. git is a versioning system to keep track of your code, while git hub is repository to store your code
+# Doris, another night! 
